ebf4332da36d2b5753f45bd17058d3de36b8439b
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-09-13 22:03:31 +0700

    Update Bai1.cpp

--- Tuan2/Bai1.cpp
+++ Tuan2/Bai1.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cin>>n;
+    int a[100];
+    for(int i=0;i<n;i++) cin>>a[i];
+    int count=0;
+    for(int i=0;i<n-1;i++) {
+        for(int j=i+1;j<n;j++) {
+            if(a[i]==a[j] && i<j) count++;
+        }
+    }
+    cout<<count;
+}
