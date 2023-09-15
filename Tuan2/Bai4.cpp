52b61d4dc8f1adad34b2964e34f22b0fbf736a63
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-09-15 14:38:05 +0700

    Update Bai4.cpp

--- Tuan2/Bai4.cpp
+++ Tuan2/Bai4.cpp
@@ -1,62 +1,91 @@
-#include <iostream>
-#include<queue>
-
+#include<iostream>
+#include<vector>
 using namespace std;
+
+class Node {
+public:
+    int value;
+    Node *next;
+
+    Node(int value) {
+        this->value = value;
+        this->next = NULL;
+    }
+};
 
 /*class Queue {
 public:
-	int *a;
-	int head, tail, size,max=10;
-	Queue() {
-		a = new int[max];
-		head = 0;
-		tail = 0;
-		size = 0;
-	}
-	
+    Node *head; //front
+    Node *rear; //rear
+
     void enqueue(int x) {
-        tail = (tail+1)%max;
-        size++;
-        a[tail] = x;
+        Node *newNode = new Node(x);
+
+        if(rear == NULL) {
+            head = rear = newNode;
+            return;
+        }
+        rear->next = newNode;
+        rear = newNode;
     }
 
     void dequeue() {
-    	if(!isEmpty()) {
-    		size--;
-    		head = (head+1)%max;
-		}
-    }
-    
-    void print() {
-    	for(int i=0;i<size;i++) {
-    		cout<<a[i]<<" ";
-		}
-	}
-    
-    bool isEmpty() {
-    	return (size==0);
-	}
-};*/
-
-int main() {
-    queue<int> qq;
-    int n, x;
-    string op;
-
-    cin >> n;
-    for(int i=0; i<n; i++) {
-        cin >> op;
-        if(op == "enqueue") {
-            cin >> x;
-            qq.push(x);
-        } else if(op == "dequeue") {
-            qq.pop();
+        if(head == NULL) {
+            return;
+        }
+        head = head->next;
+        if(head==NULL) {
+            rear = NULL;
         }
     }
+
+    void print() {
+        Node *node = head;
+        while(node->next!=NULL) {
+            cout<<node->value<<" ";
+            node = node->next;
+        }
+        cout<<endl;
+    }
+};*/
+
+class Queue {
+private:
+    vector<int> v;
+public:
+    void enqueue(int x) {
+        v.push_back(x);
+    }
+
+    void dequeue() {
+        v.erase(v.begin());
+    }
+
+    void print() {
+        for(int i=0;i<v.size();i++) {
+            cout<<v[i]<<" ";
+        }
+    }
+};
+
+int main() {
+    Queue q;
+
+    int n;
+    cin>>n;
+    while(n--) {
+        int x;
+        string op;
+        cin>>op;
+        if(op=="enqueue") {
+            cin>>x;
+            q.enqueue(x);
+        }else if(op=="dequeue") {
+            q.dequeue();
+        }
+    }
+    
+    q.print();
 	
-	while(!qq.empty()) {
-		cout<<qq.front()<<" ";
-		qq.pop();
-	}
-	
+    return 0;
 }
