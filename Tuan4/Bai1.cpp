08671875fd41548eb850615876b4b58b8db5dc7f
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-10-23 21:57:04 +0700

    Update Bai1.cpp

--- Tuan4/Bai1.cpp
+++ Tuan4/Bai1.cpp
@@ -1,1 +1,4 @@
-
+1.Tốc độ tính toán: Một hàm băm tốt phải tính toán rất nhanh.
+2.Giảm thiểu xung đột: Hàm băm sẽ giảm thiểu sự trùng lặp của các giá trị đầu ra, còn được gọi là xung đột. 
+  Điều này có nghĩa là dữ liệu đầu vào khác nhau sẽ ánh xạ lý tưởng tới các giá trị băm khác nhau, làm giảm 
+  khả năng hai phần dữ liệu khác nhau tạo ra cùng một giá trị băm.
